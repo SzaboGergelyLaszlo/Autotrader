ca7e70749a3c401f2de841249b0ec5f085ef9d7e
Szabó Gergely László <szabogl@KKSZKI.LOCAL>
2025-02-13 18:55:42 +0100

    Get all cars, Add new car

---
+++ autotrader/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AddNewCar from './components/AddNewCar';
+import GetallCars from './components/GetAllCar';
+
+function App() {
+  return (
+   <div className='container'>
+    <AddNewCar/>
+    <GetallCars/>
+   </div>
+  );
+}
+
+export default App;
---
+++ autotrader/src/components/GetAllCar.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+
+function GetallCars() {
+    const url = "https://localhost:7240/cars"
+    const [carData, setCardata] = useState([])
+
+        useEffect(() => {
+            (async () => {
+                const request = await fetch(url, {
+
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+
+                if(!request.ok){
+                    console.log("Hiba")
+                    return
+                }
+
+                const response = await request.json();
+                setCardata(response.result)
+                console.log(response.meesage)
+            })()
+        }, [carData])
+
+        const carElements = carData.map(
+            car =>{
+                return(
+                    <div class="card m-3 pt-2" style={{'width' : 200, 'float': 'left'}}>
+                       <div class="card-header">{car.brand}</div>
+                       <div class="card-body">{car.type}</div>
+                       <div class="card-body">{car.color}</div>
+                       <div class="card-footer">{car.myear}</div>
+                    </div>
+                )
+            }
+        )
+
+    return (
+        <div>{carElements}</div>
+    )
+}
+
+export default GetallCars
